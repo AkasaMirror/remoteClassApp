d6ef54a9d4a547cddbd111ce923ccb99d11e9290
原如矢 <hy@harajoyanoMacBook-Air.local>
2023-10-27 14:45:12 +0900

    second-commit

---
+++ src/main.js
@@ -0,0 +1,55 @@
+import { nowInSec, SkyWayAuthToken, SkyWayContext, SkyWayRoom, SkyWayStreamFactory, uuidV4 } from '@skyway-sdk/room';
+
+const token = new SkyWayAuthToken({
+    jti: uuidV4(),
+    iat: nowInSec(),
+    exp: nowInSec() + 60 * 60 * 24,
+    scope: {
+        app: {
+            id: '7ab8b490-382b-492e-bb0a-9324c3f9f86a',
+            turn: true,
+            actions: ['read'],
+            channels: [
+            {
+                id: '*',
+                name: '*',
+                actions: ['write'],
+                members: [
+                    {
+                        id: '*',
+                        name: '*',
+                        actions: ['write'],
+                        publication: {
+                            actions: ['write'],
+                        },
+                        subscription: {
+                            actions: ['write'],
+                        },
+                    },
+                ],
+                sfuBots: [
+                    {
+                        actions: ['write'],
+                        forwardings: [
+                            {
+                                actions: ['write'],
+                            },
+                        ],
+                    },
+                ],
+            },
+            ],
+        },
+    },
+  
+}).encode('S6MaSxEOJoQlM9JnfgowvhVbdsWIV9bz4WKW7fsftzc=');
+
+(async () => {
+    // 1
+    const localVideo = document.getElementById('local-video');
+  
+    const { audio, video } = await SkyWayStreamFactory.createMicrophoneAudioAndCameraStream(); // 2
+  
+    video.attach(localVideo); // 3
+    await localVideo.play(); // 4
+})(); // 1
